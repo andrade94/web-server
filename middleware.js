7e24c5f9f544e4240ba93bf069eca578522bdfab
andrade94 <eduardo.mtz9@gmail.com>
2017-01-23 17:24:19 -0600

    Changes in middleware file

---
+++ middleware.js
@@ -0,0 +1,15 @@
+// Use module exports to exports middleware
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('Private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		var date = new Date().toString();
+		console.log('Request: ' + req.method + ' ' + req.originalUrl + ' in ' + date);
+		next();
+	}
+};
+
+module.exports = middleware;
